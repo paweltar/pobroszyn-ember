d9d340c3f5f85ebc1f753df1c7e8906f9398efef
paweltar <paweltar@gmail.com>
2017-03-06 21:56:35 +0100

    Add slick slider component without styles and proper content

--- app/components/block-gallery.js
+++ app/components/block-gallery.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didRender() {
+    this._super();
+    console.log('no elo');
+    this.$('.gallery__slides').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  }
 });
