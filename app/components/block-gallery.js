1b883667fb73fa873cc50520461de6319e4bc2c4
paweltar <paweltar@gmail.com>
2017-03-06 22:07:49 +0100

    Add custom arrows to slick slider

--- app/components/block-gallery.js
+++ app/components/block-gallery.js
@@ -3,16 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didRender() {
     this._super();
-    console.log('no elo');
     this.$('.gallery__slides').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
       infinite: true,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear'
+      draggable: true,
+      prevArrow: '.slidenav--left',
+      nextArrow: '.slidenav--right'
     });
   }
 });
