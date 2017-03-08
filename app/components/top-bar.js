2a28362f02df0c2441bb662f5b8fe5ef1dacc995
paweltar <paweltar@gmail.com>
2017-03-08 14:26:09 +0100

    Include ResetScroll mixin on about us route

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didRender() {
     this._super();
-    this.$('#modal-full').on('click', function(e) {  
+    this.$('#modal-full').on('click', function() {  
        UIkit.modal('#modal-full').hide();
     });
   }
