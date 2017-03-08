e81c9505a9238ff2ffe01c7196300909feb55075
paweltar <paweltar@gmail.com>
2017-03-08 20:53:31 +0100

    Update gallery

--- app/components/muuri-gallery.js
+++ app/components/muuri-gallery.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement() {
+    this._super();
+      var grid = UIkit.grid(UIkit.$('#gallery__grid'));
+      grid.updateLayout()
+  }
 });
