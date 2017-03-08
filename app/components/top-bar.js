4e173167149f6212aad144ef3235912bb8b08a97
paweltar <paweltar@gmail.com>
2017-03-08 13:13:49 +0100

    Fix modal menu hiding

--- app/components/top-bar.js
+++ app/components/top-bar.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didRender() {
+    this._super();
+    this.$('#modal-full').on('click', function(e) {
+       $('#modal-full').fadeOut(300);  
+       UIkit.modal('#modal-full').hide();
+    });
+  }
 });
