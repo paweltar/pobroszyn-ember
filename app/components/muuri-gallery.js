63162c2d7b33526199ea9da7d4e536833db768e4
paweltar <paweltar@gmail.com>
2017-03-09 09:07:53 +0100

    Clean JShint errors and unnecessary code

--- app/components/muuri-gallery.js
+++ app/components/muuri-gallery.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement() {
-    this._super();
-      var grid = UIkit.grid(UIkit.$('#gallery__grid'));
-      grid.updateLayout()
-  }
 });
